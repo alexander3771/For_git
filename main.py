510bcecc91f4f4ee071ade63e18e43d7ef185f5f
alexander3771 <alexemfan@mail.ru>
2015-03-06 16:30:03 +0300

    Add fib function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 a = [i+j for i in xrange(4) for j in xrange(5)]
 
 print a
+
+
+def fib(n):
+    a, b = 0, 1
+    for i in xrange(n):
+        a, b = b, a+b
+        yield a
+
+c = fib(20)
+
+print list(c)
